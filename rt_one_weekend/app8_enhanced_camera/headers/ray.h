1856e4300ae335935c3061f23622159cc13c562c
omd24 <jack34169>
2021-07-05 00:03:37 +0430

    Hollow glass sphere using dielectric material

---
+++ rt_one_weekend/app8_enhanced_camera/headers/ray.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "vec3.h"
+
+typedef struct {
+    point3 origin;
+    vec3f dir;
+} ray;
+
+// returns: P(t) = A + tB
+// A : origin, B: dir
+inline point3
+ray_at (ray * r, float t) {
+    vec3f tB = vec3_scale(r->dir, t);
+    point3 P = vec3_add(r->origin, tB);
+    return P;
+}
+
